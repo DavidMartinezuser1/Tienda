82e8f6eddba7ea7ad9574af1e215f7282bb8780c
David Martinez <davidmatinesfa@gmail.com>
2025-07-06 19:05:12 -0600

    Semana 6
    Contiene el proyecto funcionando con Firebase y

---
+++ Tienda1/src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,23 @@
+package com.tienda.service;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-1.firebasestorage.app";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-1-firebase-adminsdk-fbsvc-9c5dc98c0e"+".json";
+}
+
+
