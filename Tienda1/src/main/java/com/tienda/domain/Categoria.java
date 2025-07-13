984a2882407c716902b41846bc8713500e204700
David Martinez <davidmatinesfa@gmail.com>
2025-07-12 18:43:10 -0600

    Semana 9
    En este proyecto esta los datos de semana 9

--- Tienda1/src/main/java/com/tienda/domain/Categoria.java
+++ Tienda1/src/main/java/com/tienda/domain/Categoria.java
@@ -2,6 +2,8 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
@@ -17,6 +19,10 @@ public class Categoria implements Serializable {
      @Column(name="ruta_imagen")
     private String rutaImagen;
     private boolean activo;
+
+    @OneToMany
+    @JoinColumn(name="id_categoria", updatable = false)
+    List<Producto> productos;
 
     public Categoria( String descripcion, boolean activo) {
         this.descripcion = descripcion;
